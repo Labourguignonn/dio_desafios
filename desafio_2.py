b34cbd430e006d300df2574f4046a5b75d8419e9
Lucas Araujo Bourguignon <labourguginon@gmail.com>
2024-05-01 00:03:16 -0300

    modified:   desafio_2.py

--- desafio_2.py
+++ desafio_2.py
@@ -1,25 +1,24 @@
 qt_vitorias = int(input())
 qt_derrotas = int(input())
-nível = ""
 
-def classificador(vit):
+def classificador(rank):
         nível = ''
-        if vit <= 10:
+        if rank <= 10:
             nível = "Ferro"
-        elif  vit > 10 and vit <= 20:
+        elif  rank > 10 and rank <= 20:
             nível = "Bronze"
-        elif  vit > 20 and vit <= 50:
+        elif  rank > 20 and rank <= 50:
             nível = "Prata" 
-        elif  vit > 50 and vit <= 80:
+        elif  rank > 50 and rank <= 80:
             nível = "Ouro"
-        elif  vit > 80 and vit <= 90:
+        elif  rank > 80 and rank <= 90:
             nível = "Diamante"
-        elif  vit > 90 and vit <=100:
+        elif  rank > 90 and rank <=100:
             nível = "Lendário"
         else:
             nível = "Imortal"    
         return nível
-def vitoriasRankeadas(vit,der):
+def rankoriasRankeadas(vit,der):
      return vit - der
 
-print(f"O Herói tem de saldo de {vitoriasRankeadas(qt_vitorias,qt_derrotas)} está no nível de {classificador(qt_vitorias)}")
+print(f"O Herói tem de saldo de {rankoriasRankeadas(qt_vitorias,qt_derrotas)} está no nível de {classificador(rankoriasRankeadas(qt_vitorias,qt_derrotas))}")
